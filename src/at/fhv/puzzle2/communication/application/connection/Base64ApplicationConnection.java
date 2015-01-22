c99d06e3973a839306eae22920c737380f8c7863
Andreas Sinz <andreas.sinz@aon.at>
2015-01-22 18:26:32 +0100

    Updated

--- src/at/fhv/puzzle2/communication/application/connection/Base64ApplicationConnection.java
+++ src/at/fhv/puzzle2/communication/application/connection/Base64ApplicationConnection.java
@@ -25,7 +25,7 @@ public class Base64ApplicationConnection extends ApplicationConnectionDecorator 
 
         message.setMessage(Base64.getDecoder().decode(message.getMessage()));
 
-        System.out.println("#############Received app-message: " + message.getMessage());
+        System.out.println("\nReceived app-message: " + message.getMessage());
 
         return message;
     }
