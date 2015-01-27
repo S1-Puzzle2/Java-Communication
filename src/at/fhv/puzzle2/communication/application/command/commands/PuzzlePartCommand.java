fba1305d009db7fdc779620b3b323bd7bd0162e3
Andreas Sinz <andreas.sinz@aon.at>
2015-01-27 22:44:39 +0100

    Use byte-array instead of Base64 string inside PuzzlePartCommand and added the order-int, which is needed by the unity client

--- src/at/fhv/puzzle2/communication/application/command/commands/PuzzlePartCommand.java
+++ src/at/fhv/puzzle2/communication/application/command/commands/PuzzlePartCommand.java
@@ -5,26 +5,34 @@ import at.fhv.puzzle2.communication.application.command.Command;
 import at.fhv.puzzle2.communication.application.command.constants.CommandConstants;
 import at.fhv.puzzle2.communication.application.command.constants.CommandType;
 
+import java.util.Base64;
+
 public class PuzzlePartCommand extends Command {
-    private String _image64;
+    private byte[] _image;
     private int _imageID;
+    private int _order;
 
     public PuzzlePartCommand(ClientID clientID) {
         super(clientID, CommandType.PuzzlePart);
     }
 
-    public void setImage(String image64) {
-        _image64 = image64;
+    public void setImage(byte[] image) {
+        _image = image;
     }
 
     public void setImageID(int id) {
         _imageID = id;
     }
 
+    public void setOrder(int order) {
+        _order = order;
+    }
+
     @Override
     public String toJSONString() {
-        _messageData.put(CommandConstants.IMAGE, _image64);
+        _messageData.put(CommandConstants.IMAGE, Base64.getEncoder().encode(_image));
         _messageData.put(CommandConstants.PUZZLE_PART_ID, _imageID);
+        _messageData.put(CommandConstants.PUZZLE_PART_ORDER, _order);
 
         return super.toJSONString();
     }
